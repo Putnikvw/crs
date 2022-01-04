488db4d2bbb348c493c557d3cb9304eeba707cc3
perun <igor.work15@gmail.com>
2022-01-04 20:31:15 +0200

    Added mapper unit test

---
+++ src/test/java/com/cloudmore/mapper/ClientMapperTest.java
@@ -0,0 +1,59 @@
+package com.cloudmore.mapper;
+
+import com.cloudmore.builder.ClientBuilder;
+import com.cloudmore.domain.Client;
+import com.cloudmore.dto.ClientDto;
+import com.cloudmore.message.model.KafkaClientMessage;
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+
+import java.math.BigDecimal;
+
+import static com.cloudmore.builder.ClientBuilder.*;
+
+/**
+ * ClientMapperTest
+ *
+ * @author zen
+ */
+public class ClientMapperTest implements WithAssertions {
+
+    private ClientMapper mapper = Mappers.getMapper(ClientMapper.class);
+
+
+    @Test
+    public void toDomainTest() {
+        ClientBuilder clientBuilder = aClient().getFullClient();
+        Client result = mapper.toDomain(clientBuilder.getClientDto());
+        Client expected = clientBuilder.getClientModel();
+
+        assertThat(result).usingRecursiveComparison().isEqualTo(expected);
+    }
+
+    @Test
+    public void toDtoTest() {
+        ClientBuilder clientBuilder = aClient()
+                .withName("Alex")
+                .withSurname(DEFAULT_SURNAME)
+                .withWage(new BigDecimal("6547.87"))
+                .withEventTime(DEFAULT_EVENT_TIME);
+        ClientDto result = mapper.toDto(clientBuilder.getClientModel());
+        ClientDto expected = clientBuilder.getClientDto();
+
+        assertThat(result).usingRecursiveComparison().isEqualTo(expected);
+    }
+
+    @Test
+    public void toKafkaMessageTest() {
+        ClientBuilder clientBuilder = aClient()
+                .withName("Marek")
+                .withSurname("Simpson")
+                .withWage(new BigDecimal("6547.87"))
+                .withEventTime(DEFAULT_EVENT_TIME);
+        KafkaClientMessage result = mapper.toKafkaMessage(clientBuilder.getClientDto());
+        KafkaClientMessage expected = clientBuilder.getKafkaMessageClient();
+
+        assertThat(result).usingRecursiveComparison().isEqualTo(expected);
+    }
+}
